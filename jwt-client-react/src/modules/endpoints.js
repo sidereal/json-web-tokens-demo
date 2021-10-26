f7f90cc659ff48b2a4e20867a17923d3274d443e
Stuart <stuart_thirkettle@eventricity.biz>
2021-10-26 16:53:07 +0100

    refactored api endpoints into endpoints.js

---
+++ jwt-client-react/src/modules/endpoints.js
@@ -0,0 +1,9 @@
+export const api = 'http://localhost:4001/api'
+
+export const login = `${api}/login`
+export const logout = `${api}/logout`
+export const refreshtokens = `${api}/refreshtokens`
+
+export const errorDemo = `${api}/demo/error`
+export const tokenSecurityDemo =  `${api}/demo/token-based-security`
+export const roleSecurityDemo =  `${api}/demo/role-based-security`
